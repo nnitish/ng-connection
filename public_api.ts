57434d70ff56f783178ee8e07d48113d7207f1ee
Nitish kumar <nitish.kumar@phasezeroventures.com>
2020-01-24 10:56:34 +0530

    service tested

---
+++ public_api.ts
@@ -0,0 +1,1 @@
+export * from './src/app/modules/ng-connection/ng-connection.module';
---
+++ src/app/modules/ng-connection/ng-connection.service.ts
@@ -0,0 +1,46 @@
+import { Injectable, EventEmitter } from '@angular/core';
+import { Observable, fromEvent } from 'rxjs';
+import {debounceTime, startWith} from 'rxjs/operators';
+
+
+@Injectable({
+    providedIn: 'root'
+})
+export class NgConnection {
+
+    private online$: Observable<any>;
+    private offline$: Observable<any>;
+
+    private statusEmitter: any = new EventEmitter();
+
+    private isConnected: boolean = true;
+
+    constructor() {
+        this.trackingConnection();
+    }
+
+    private trackingConnection(): void {
+        this.online$ = fromEvent(window, 'online');
+        this.offline$ = fromEvent(window, 'offline');
+
+        this.online$.subscribe(_ => {
+            this.isConnected = true;
+            this.emitNetworkStatus();
+        });
+
+        this.offline$.subscribe(_ => {
+            this.isConnected = false;
+            this.emitNetworkStatus();
+        });
+    }
+
+    private emitNetworkStatus(): void {
+        this.statusEmitter.emit(this.isConnected);
+    }
+
+    public connectivity(showCurrentStatus: boolean = true): Observable<boolean> {
+        return showCurrentStatus
+        ? this.statusEmitter.pipe(debounceTime(300), startWith(this.isConnected))
+        : this.statusEmitter.pipe(debounceTime(300));
+    }
+}
